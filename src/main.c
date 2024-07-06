0d7296c8ff769aa6c3e28f028e14ea95f14de244
StayInTheKitchen <kimminjun514@gmail.com>
2024-07-06 13:49:09 +0900

    CursorTest

--- src/main.c
+++ src/main.c
@@ -2,39 +2,12 @@
 #include <stdio.h>
 #include <conio.h>
 
-int main (void)
+int main(void)
 {
     int keyboardInput;
     while(1)
     {
         keyboardInput = _getch();
-
-        if (keyboardInput == 224)
-        {
-            keyboardInput = _getch();
-
-            switch (keyboardInput)
-            {
-                case 75:
-                    printf("left arrow hit\n");
-                    break;
-                case 72:
-                    printf("up arrow hit\n");
-                    break;
-                case 77:
-                    printf("right arrow hit\n");
-                    break;
-                case 80:
-                    printf("down arrow hit\n");
-                    break;
-            }
-        }
-
-        if (keyboardInput == 'A')
-        {
-            return 0;
-        }
+        printf("%d\n", keyboardInput);
     }
-
-    return 0;
 }
---
+++ src/test/MoveCursorByKeyboardInput/MCBKImain.c
@@ -0,0 +1,12 @@
+#include <Windows.h>
+#include <conio.h>
+
+void gotoXY(int inputX, int inputY)
+{
+    
+}
+
+int main(void)
+{
+    
+}
